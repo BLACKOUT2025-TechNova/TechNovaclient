5e45d4da8365d55ae808e99c8ddec1270d950a3b
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2025-01-11 14:35:08 +0900

    feat:folder structure setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/main/Main";
+import Gptapi from "./utils/Gptapi";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/gptapi" element={<Gptapi />} />
       </Routes>
     </Router>
   );
