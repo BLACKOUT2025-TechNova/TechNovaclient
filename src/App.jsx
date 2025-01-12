e6a6f242df7a90a79f9896a40a85413373646199
ji-hokim <jimmy7335@korea.ac.kr>
2025-01-12 10:07:51 +0900

    이미지 업로드 및 점수 평가 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/main/Main";
-import Gptapi from "./utils/Gptapi";
 import Mapview from "./pages/mapview/Mapview";
 import Authhunter from "./pages/authhunter/Authhunter";
 import QRScanner from "./pages/qrscanner/QRScanner";
@@ -12,7 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/gptapi" element={<Gptapi />} />
         <Route path="/mapview" element={<Mapview />} />
         <Route path="/authhunter" element={<Authhunter />} />
         <Route path="/qrscanner" element={<QRScanner />} />
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    https: true,
+    https: false,
 
     host: "0.0.0.0",
     port: 3000,
